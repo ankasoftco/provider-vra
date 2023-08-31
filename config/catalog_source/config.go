87c04ed48157e94f4da3e3d60e46084600573a99
csermet-ankasoft <csermet@ankasoft.co>
2023-08-31 10:49:47 +0300

    catalog_source bluprint/entitlement added

---
+++ config/catalog_source/config.go
@@ -0,0 +1,24 @@
+package catalogSource
+
+import "github.com/upbound/upjet/pkg/config"
+
+// Configure configures individual resources by adding custom ResourceConfigurators.
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("vra_catalog_source_blueprint", func(r *config.Resource) {
+		r.ShortGroup = "catalogsourceblueprint"
+		r.Kind = "CatalogSourceBlueprint"
+		r.Version = "v1alpha1"
+		r.References["project_id"] = config.Reference{
+			Type: "github.com/ankasoftco/upjet-provider-vra/apis/project/v1alpha1.Project",
+		}
+	})
+
+	p.AddResourceConfigurator("vra_catalog_source_entitlement", func(r *config.Resource) {
+		r.ShortGroup = "catalogsourceentitlement"
+		r.Kind = "CatalogSourceEntitlement"
+		r.Version = "v1alpha1"
+		r.References["project_id"] = config.Reference{
+			Type: "github.com/ankasoftco/upjet-provider-vra/apis/project/v1alpha1.Project",
+		}
+	})
+}
